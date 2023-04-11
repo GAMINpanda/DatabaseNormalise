60c357cabf92b7a844a9e5235dcd3a234a339554
Alexander Mitchiner <mitchinerfamily1@gmail.com>
2023-04-11 16:54:16 +0100

    adding files

---
+++ TableTestWithPrimary.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Customers (
+    [ID] INT,
+    [Name] VARCHAR(100),
+    [Industry] VARCHAR(100),
+    [Project1_ID] INT,
+    [Project1_Feedback] TEXT,
+    [Project2_ID] INT,
+    [Project2_Feedback] TEXT,
+    [ContactPersonID] INT,
+    [ContactPersonAndRole] VARCHAR(255),
+    [PhoneNumber] VARCHAR(12),
+    [Address] VARCHAR(255),
+    [City] VARCHAR(255),
+    [Zip] VARCHAR(5)
+    PRIMARY KEY [ID]
+  )
+ GO
